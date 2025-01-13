3535d373aaacefda6410583d42ad8cf10c2d4d78
Pratik7233 <pratikberad999@gmail.com>
2025-01-13 23:01:23 +0530

    done
    finish

--- OUR STORY/OUR STORY.php
+++ OUR STORY/OUR STORY.php
@@ -3,123 +3,131 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Our Story</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-<a href="#"><img src="../OUR STORY/photos/Blogmain1.jpg" style="height:500px; width: 100%;"></a>
-<div class="marquee">
-  <span>
+    <a href="#"><img src="../OUR STORY/photos/Blogmain1.jpg" class="img-fluid" alt="..."></a>
     
-   <h3><b> <span class="icon"><i class="fa-solid fa-bolt"></i> Holistic Approach to Oral Care </span><span class="icon"><i class="fa-solid fa-bolt"></i>
-    Sustainable and Eco-Friendly </span> <span class="icon"><i class="fa-solid fa-bolt"></i>
-    Technology-Driven and Research-Backed </span><span class="icon"><i class="fa-solid fa-bolt"></i>
-    Enhanced Plaque-Removal </span><span class="icon"><i class="fa-solid fa-bolt"></i>
-    Recommended by Experts </span> </b></h3>
-  </span>
-</div>
-<img src="../OUR STORY/photos/blog2.jpg" style="height: 300px; width: 100%;">
-<img src="../OUR STORY/photos/blog3.jpg" style="height: 300px; width: 100%;">
-<img src="../OUR STORY/photos/blog4.jpg" style="height: 300px; width: 100%;">
-
-<div class="main">
-  <h1 style="text-align: center; color: black;"><b>Why Us</b></h1>
-  <div class="container1" style="width: 50%;float: left;padding-left: 25px;">
-    <img src="../OUR STORY/photos/shop.jpg" style="height: 400px;width: 100%; "></div>
-    <div class="container2" style="width: 48%;background-color:skyblue;float: left;height: 400px;">
-      <h2 style="text-align: center;color: black; margin-top: 80px;"><b>Because we asked you a critical question!</b></h2>
-      <br>
-      <h5 style="text-align: center;color: black;">How does your oral health influence your overall health?</h5>
-      <br>
-      <p style="width: auto;color: black;text-align: center;margin-left: 7px;margin-right: 7px;">It turns out that the bacteria cells residing in your mouth can weaken your immune system and can also spur diseases like heart disease, cancer, diabetes and alzheimer's. For years, we've been using the same oral care products without asking for the same accountability we ask for in our foods and skincare products.</p>
-
+    
+    <div class="marquee">
+      <span>
+        <h3>
+          <b> 
+            <span class="icon"><i class="fa-solid fa-bolt"></i> Holistic Approach to Oral Care </span>
+            <span class="icon"><i class="fa-solid fa-bolt"></i> Sustainable and Eco-Friendly </span>
+            <span class="icon"><i class="fa-solid fa-bolt"></i> Technology-Driven and Research-Backed </span>
+            <span class="icon"><i class="fa-solid fa-bolt"></i> Enhanced Plaque-Removal </span>
+            <span class="icon"><i class="fa-solid fa-bolt"></i> Recommended by Experts </span> 
+          </b>
+        </h3>
+      </span>
+    </div>
+    <img src="../OUR STORY/photos/blog2.jpg" class="img-fluid" alt="...">
+    <img src="../OUR STORY/photos/blog3.jpg" class="img-fluid" alt="...">
+    <img src="../OUR STORY/photos/blog4.jpg" class="img-fluid" alt="...">
+ 
+    <div class="text">
+      <h1 style="text-align: center; color: black;"><b>Why Us</b></h1>
+    
+    <div class="main">
+      <div class="container d-flex flex-wrap">
+        <div class="container1" style="flex: 1 1 50%; padding-left: 25px;">
+          <img src="../OUR STORY/photos/shop.jpg" style="height: 400px;width: 100%; ">
+        </div>
+        <div class="container2" style="flex: 1 1 50%; background-color:skyblue; padding: 25px;">
+          <h2 style="text-align: center;color: black; margin-top: 80px;"><b>Because we asked you a critical question!</b></h2>
+          <h5 style="text-align: center;color: black;">How does your oral health influence your overall health?</h5>
+          <p style="color: black; text-align: center; margin: 7px;">It turns out that the bacteria cells residing in your mouth can weaken your immune system and can also spur diseases like heart disease, cancer, diabetes and Alzheimer's. For years, we've been using the same oral care products without asking for the same accountability we ask for in our foods and skincare products.</p>
+        </div>
+    
+      
+      <h5 style="color: black;text-align: center;margin-top: 10px;">What if your oral care products were actually designed with a holistic approach to help you improve your overall oral health including your symbiotic oral microbiome?</h5>
+      <div class="containt">
+        <h1 style="color: black;text-align: center;margin-top: 15px;margin-left: 180px;"><b>So, let us ask you another question</b></h1>
+      </div>
+    </div>
+    </div>
     </div>
     <br>
-    
-    <h5 style="color: black;text-align: center;margin-top: 10px;">What if your oral care products were actually designed with a holistic approach to help you improve your overall oral health including your symbiotic oral microbiome?</h5>
     <br>
-    <div class="containt">
-    <h1 style="color: black;text-align: center;margin-top: 15px;"><b>So, let us ask you another question</b></h1>
-</div>
-</div>
-<br>
-<br>
-<div class="submain">
-  <div class="container1" style="width:50%;background-color:skyblue;float: left;height: 400px;padding-left: 25px;margin-top: 20px;">
-<h5 style="color: black; margin-top: 65px;font-family: Arial, Helvetica, sans-serif;">Do you believe that the products available in the market genuinely focus on improving oral health?</h5>
-<p style="color: black;margin-top: 50px;margin-right: 7px;">To your surprise, most of the oral care products in the market today are created using cheap ingredients and synthetic chemicals. We're setting out to change that because the mouth is too critical to ignore for your overall health. So, to solve this exact problem. we started collaborating with multiple dentists, consumers and product designers to create a line of eco - friendly, ergonomic, organic, and technology forward oral healthcare products.</p>
-</div>
 
-<div class="container2" style="width: 50%;float: left;padding-right: 25px;margin-top: 20px;">
-  <img src="../OUR STORY/photos/shop2.jpg" style="height: 400px;width: 100%; ">
-</div>
-</div>
-<br>
-<br>
-<div class="main1">
-  <div class="container1" style="width: 50%;float: left;padding-left: 25px;margin-top: 20px;">
-    <img src="../OUR STORY/photos/shop3.jpg" style="height: 400px;width: 100%; "></div>
-    <div class="container2" style="width: 48%;background-color:skyblue;float: left;height: 400px;margin-top: 20px;">
-      <h5 style="text-align: center;color: black;font-family: Arial, Helvetica, sans-serif;">Thus, Dr. Dento was born!</h5>
-      <br>
-      <p style="width: auto;color: black;text-align: center;margin-left: 7px;margin-right: 7px;">All we want is for you to flaunt your smile unapologetically and our range of effective care products is here to make it possible. Designed with the utmost care and precision, adhering to the highest standard of quality, our products offer 100 % plaque removal and a holistic brushing experience, therefore helping you elevate your oral health.</p>
+    <div class="submain">
+      <div class="container3 d-flex flex-wrap">
+        <div class="container4" style="flex: 1 1 50%; background-color:skyblue; padding-left: 25px;">
+          <h5 style="color: black; margin-top: 65px; font-family: Arial, Helvetica, sans-serif;">Do you believe that the products available in the market genuinely focus on improving oral health?</h5>
+          <p style="color: black; margin-top: 50px; margin-right: 7px;">To your surprise, most of the oral care products in the market today are created using cheap ingredients and synthetic chemicals. We're setting out to change that because the mouth is too critical to ignore for your overall health. So, to solve this exact problem, we started collaborating with multiple dentists, consumers and product designers to create a line of eco-friendly, ergonomic, organic, and technology-forward oral healthcare products.</p>
+        </div>
+        <div class="container5" style="flex: 1 1 50%; padding-right: 25px;">
+          <img src="../OUR STORY/photos/shop2.jpg" style="height: 400px;width: 100%; ">
+        </div>
+      </div>
     </div>
-</div>
-<br>
-<br>
-<div class="Big">
-<h2 style="text-align: center;color: black;font-family: Arial, Helvetica, sans-serif;"><b>Redefine Your Oral Care</b></h2>
-<h2 style="text-align: center;color: black;font-family: Arial, Helvetica, sans-serif;"><b>Innovatively curated oral care that is</b></h2>
-<div class="img" style="float: left;">
-<a href="#"><img src="../OUR STORY/photos/Cfree.jpg" style="height: 250px;width: 250px;margin-left: 110px;;border-radius: 50%;background-color: white;">
-<h4 style="color: black;margin-left: 150px;margin-top: 10px;"><b>Cruelty Free</b></h4>  
-</a></div>
-<div class="img1" style="float: left;">
-  <a href="#"><img src="../OUR STORY/photos/dentist.jpg" style="height: 250px;width: 250px;margin-left: 30px;border-radius: 50%;background-color: white;">
-  <h4 style="color: black;margin-left: 30px;margin-top: 10px;"><b>Dentist Recommended</b></h4>  
-</a>
-</div>
-<div class="img2" style="float: left;">
-  <a href="#"><img src="../OUR STORY/photos/eco.jpg" style="height: 250px;width: 250px;margin-left: 30px;border-radius: 50%;background-color: white;">
-    <h4 style="color: black;margin-left: 90px;margin-top: 10px;"><b>Eco Friendly</b></h4>  
-   </a></div>
-   <div class="img3" style="float: left;">
-    <a href="#"><img src="../OUR STORY/photos/design.jpg" style="height: 250px;width: 250px;margin-left: 30px;;border-radius: 50%;background-color: white;">
-      <h4 style="color: black;margin-left: 60px;margin-top: 10px;"><b>Human Centric Design</b></h4>
-      </a></div>
-      <br>
-      <br>
-     <div class="img4" style="float: left;"> 
-      <a href="#"><img src="../OUR STORY/photos/vegan.jpg" style="height: 250px;width: 250px;margin-left: 200px;margin-top: 40px;border-radius: 50%;background-color: white;">
-        <h3 style="color: black;margin-left: 280px;margin-top: 10px;"><b>Vegan</b></h3>
-      </a>
-     </div>
-     <div class="img5" style="float: left;"> 
-      <a href="#"><img src="../OUR STORY/photos/tech.jpg" style="height: 250px;width: 250px;margin-left: 50px;margin-top: 40px;border-radius: 50%;background-color: white;">
-        <h3 style="color: black;margin-left: 70px;margin-top: 10px;"><b>Technology Driven</b></h3>
-      </a>
-     </div>
-     <div class="img6" style="float: left;"> 
-      <a href="#"><img src="../OUR STORY/photos/toxic.jpg" style="height: 250px;width: 250px;margin-left: 50px;margin-top: 40px;border-radius: 50%;background-color: white;">
-        <h3 style="color: black;margin-left: 110px;margin-top: 10px;"><b>Toxic-Free</b></h3>
-      </a>
-     </div>
+    
+    <br>
+    <br>
 
-     
-     
-       
+    <div class="main1">
+      <div class="container d-flex flex-wrap">
+        <div class="container1" style="flex: 1 1 50%; padding-left: 25px;">
+          <img src="../OUR STORY/photos/shop3.jpg" style="height: 400px;width: 100%; ">
+        </div>
+        <div class="container6" style="flex: 1 1 50%; background-color:skyblue; padding: 25px;">
+          <h5 style="text-align: center;color: black;font-family: Arial, Helvetica, sans-serif;">Thus, Dr. Dento was born!</h5>
+          <p style="color: black;text-align: center; margin: 7px;">All we want is for you to flaunt your smile unapologetically and our range of effective care products is here to make it possible. Designed with the utmost care and precision, adhering to the highest standard of quality, our products offer 100% plaque removal and a holistic brushing experience, therefore helping you elevate your oral health.</p>
+        </div>
+      </div>
+    </div>
+    
+    <br>
+    <br>
 
-
-
-</div>
-</div>
-
-
-
-
+    <div class="Big">
+      <div class="container-fluid">
+        <h2 style="text-align: center;color: black;font-family: Arial, Helvetica, sans-serif;"><b>Redefine Your Oral Care</b></h2>
+      <h2 style="text-align: center;color: black;font-family: Arial, Helvetica, sans-serif;"><b>Innovatively curated oral care that is</b></h2>
+      <div class="container d-flex flex-wrap justify-content-center">
+        <div class="img">
+          <a href="#"><img src="../OUR STORY/photos/Cfree.jpg" style="height: 250px;width: 250px; border-radius: 50%; background-color: white;">
+          <h4 style="color: black; margin-top: 10px;"><b>Cruelty Free</b></h4>  
+          </a>
+        </div>
+        <div class="img1">
+          <a href="#"><img src="../OUR STORY/photos/dentist.jpg" style="height: 250px;width: 250px; border-radius: 50%; background-color: white;">
+          <h4 style="color: black; margin-top: 10px;"><b>Dentist Recommended</b></h4>  
+          </a>
+        </div>
+        <div class="img2">
+          <a href="#"><img src="../OUR STORY/photos/eco.jpg" style="height: 250px;width: 250px; border-radius: 50%; background-color: white;">
+          <h4 style="color: black; margin-top: 10px;padding-left: 50px;"><b>Eco Friendly</b></h4>  
+          </a>
+        </div>
+        <div class="img3">
+          <a href="#"><img src="../OUR STORY/photos/design.jpg" style="height: 250px;width: 250px; border-radius: 50%; background-color: white;">
+          <h4 style="color: black; margin-top: 10px;"><b>Human Centric Design</b></h4>
+          </a>
+        </div>
+        <div class="img4">
+          <a href="#"><img src="../OUR STORY/photos/vegan.jpg" style="height: 250px;width: 250px; border-radius: 50%; background-color: white;">
+          <h3 style="color: black; margin-top: 10px;margin-left: 70px;"><b>Vegan</b></h3>
+          </a>
+        </div>
+        <div class="img5">
+          <a href="#"><img src="../OUR STORY/photos/tech.jpg" style="height: 250px;width: 250px; border-radius: 50%; background-color: white;">
+          <h3 style="color: black; margin-top: 10px;"><b>Technology Driven</b></h3>
+          </a>
+        </div>
+        <div class="img6">
+          <a href="#"><img src="../OUR STORY/photos/toxic.jpg" style="height: 250px;width: 250px; border-radius: 50%; background-color: white;">
+          <h3 style="color: black; margin-top: 10px;padding-left: 60px;"><b>Toxic-Free</b></h3>
+          </a>
+        </div>
+      </div>
+    </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+ </body>
 </html>
