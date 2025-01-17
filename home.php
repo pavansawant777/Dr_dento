7bf6f8396aa1133f8a171e7d5cd132901afe9330
shraddhaajabe <ajabeshraddha24@gmail.com>
2025-01-17 10:59:46 +0530

    reg

--- home.php
+++ home.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if user is logged in
 if (!isset($_SESSION['id'])) {
-    header("Location: login.php"); // Redirect to login page if not logged in
+    header("Location:header.php"); // Redirect to login page if not logged in
     exit;
 }
 ?>
