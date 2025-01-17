1d90109d7c7c74d08667e9ea6b3dddefb1bc03be
shraddhaajabe <ajabeshraddha24@gmail.com>
2025-01-17 12:46:14 +0530

    bybo

--- BYBO/All.php
+++ BYBO/All.php
@@ -1,3 +1,5 @@
+<?php include 'header.php';?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -119,3 +121,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
  </body>
 </html>
+<?php include 'footer.php';?>
